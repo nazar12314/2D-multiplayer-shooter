dd27fd91654fe3d54f443c53e3e18ae564403ea7
qLate <uaolegikua@gmail.com>
2024-04-03 08:56:51 +0300

    fixes

--- include/Core/FPSCounter.h
+++ include/Core/FPSCounter.h
@@ -6,7 +6,7 @@ class FPSCounter
 	inline static float lastTime = 0;
 	inline static int frames = 0;
 
-	constexpr static float FPS_PRINT_DELAY = 1;
+	constexpr static float FPS_UPDATE_DELAY = 0.3f;
 	inline static float lastFPSPrintTime = 0;
 
 public:
@@ -16,14 +16,14 @@ public:
 	static void tick()
 	{
 		frames++;
-		if (Time::time - lastTime >= 1)
+		if (Time::time - lastTime >= FPS_UPDATE_DELAY)
 		{
 			fps = frames / (Time::time - lastTime);
 			lastTime = Time::time;
 			frames = 0;
 		}
 
-		if (showFPS && Time::time - lastFPSPrintTime >= FPS_PRINT_DELAY)
+		if (showFPS && Time::time - lastFPSPrintTime >= FPS_UPDATE_DELAY)
 		{
 			lastFPSPrintTime = Time::time;
 			std::cout << "FPS: " << fps << std::endl;
